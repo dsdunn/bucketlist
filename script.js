a8b394200eb4c9c697af0173a07579fc58cacc2e
dsdunn <dsdunnmusic@gmail.com>
2018-09-04 09:32:38 -0600

    Write event listener and post request

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ $('form').submit((event) => {
   event.preventDefault();
   const title = $('#item-title').val();
   const body = $('#item-body').val();
-  
+  const info = { title, body };
 
+  const id = fetch('./ideas', {
+    method: 'POST',
+    body: JSON.stringify(info),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response)
+  .catch(err => console.log(err))
+  
+  const card = `
+  <div class="card">
+    <button class="delete-button">delete</button>
+    <h3 class="card-title">${title}</h3>
+    <p class="card-body">${body}</p>
+  </div>
+  `
 
 })
 
